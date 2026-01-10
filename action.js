300ba48a996678f0a3861006efd2b0950fcb4bc9
Andrey Helldar <helldar@dragon-code.pro>
2026-01-10 15:36:20 +0300

    Remove GitHub Action-specific implementation and associated helpers

--- action.js
+++
@@ -1,7 +0,0 @@
-const core = require('@actions/core')
-const { run } = require('@probot/adapter-github-actions')
-const previewUpdater = require('./index')
-
-run(previewUpdater).catch(error => {
-    core.setFailed(`💥 Preview Updater failed with error: ${ error.message }`)
-})
--- index.js
+++
@@ -1,9 +0,0 @@
-const { runnerIsActions } = require('./lib/detectors')
-
-module.exports = (app, { getRouter }) => {
-    if (! runnerIsActions() && typeof getRouter === 'function') {
-        getRouter().get('healthz', (request, response) => {
-            response.status(200).json({ status: 'pass' })
-        })
-    }
-}
