720ce8b12bea78c9119529d61dbeab1de4df9cc9
Andrey Helldar <helldar@dragon-code.pro>
2026-01-12 18:24:38 +0300

    Refactor `Image` type: add `ImageParameters` interface for detailed parameter typing and update defaults in `Config`

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,7 +1,22 @@
+export interface ImageParameters
+{
+    pattern: string;
+    style: string;
+    fontSize: string;
+    icon: string;
+
+    packageManager: 'composer' | 'npm' | 'yarn' | 'auto' | 'none';
+    packageName: string;
+    packageGlobal: boolean;
+
+    title: string;
+    description: string;
+}
+
 export interface Image
 {
     url: string;
-    parameters: object;
+    parameters: ImageParameters;
 }
 
 export interface Config
@@ -14,16 +29,16 @@ export const defaultConfig: Config = {
         url: 'https://banners.beyondco.de/{title}.png',
 
         parameters: {
-            theme: 'light',
             pattern: 'topography',
             style: 'style_2',
             fontSize: '100px',
             icon: 'https://laravel.com/img/logomark.min.svg',
 
-            packageManager: 'none',
+            packageManager: 'auto',
             packageName: '',
             packageGlobal: false,
 
+            title: '',
             description: ''
         }
     }
