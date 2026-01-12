74cd0565b2331e2ab38123b7185b3195ff932be1
Andrey Helldar <helldar@dragon-code.pro>
2026-01-12 18:24:42 +0300

    Refactor `packageManagers.ts`: update package manager checks to use functions for consistent invocation

--- src/utils/packageManagers.ts
+++ src/utils/packageManagers.ts
@@ -1,6 +1,5 @@
 import { fileExists } from './filesystem'
 
-export const hasComposer = fileExists('composer.json')
-export const hasNpm = fileExists('package.json')
-export const hasYarn = fileExists('yarn.lock')
-export const hasPnpm = fileExists('pnpm-lock.yaml')
+export const hasComposer = (): boolean => fileExists('composer.json')
+export const hasNpm = (): boolean => fileExists('package.json')
+export const hasYarn = (): boolean => fileExists('yarn.lock')
